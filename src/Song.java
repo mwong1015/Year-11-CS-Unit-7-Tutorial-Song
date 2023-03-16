cff60d2324fc053ed051269e52e4be611bba18f4
mwong1015 <marcowong1015@gmail.com>
2023-03-16 12:37:35 +0800

    Song

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,35 @@
+import java.util.ArrayList;
+import java.util.Locale;
+
 public class Song {
 
-    private String
+    private String title;
+    private String artist;
+    private ArrayList<String> viewers = new ArrayList<>();
+
+    public Song(String title, String artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+    public int howMany(ArrayList<String> peopleList ){ //
+        int uniqueListeners=0;
+        for(int i=0; i< peopleList.size(); i++){
+            if(!(viewers.contains(peopleList.get(i).toLowerCase()))){
+                viewers.add(peopleList.get(i).toLowerCase());
+                uniqueListeners++;
+            }
+
+        }
+        return uniqueListeners;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
 }
